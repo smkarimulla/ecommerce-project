457098ed9a1512bbd66a8da9ab69283f1e6f3394
smkarimulla <skmullakarimulla@gmail.com>
2025-11-29 18:34:24 +0530

    Update links to match the router

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<HomePage />}></Route>
+      <Route index element={<HomePage />}></Route>
       <Route path='/checkout' element={<Checkout />}></Route>
     </Routes>
   )

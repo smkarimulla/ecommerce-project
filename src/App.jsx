be574dab319156d04ac2d95411abd58186f6b5b3
smkarimulla <skmullakarimulla@gmail.com>
2025-11-29 18:44:09 +0530

    Moved orders to React

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { HomePage } from './pages/HomePage'
 import { Checkout } from './pages/Checkout'
 
 import './App.css'
+import { Orders } from './pages/Orders'
 
 function App() {
 
   return (
     <Routes>
-      <Route index element={<HomePage />}></Route>
-      <Route path='/checkout' element={<Checkout />}></Route>
+      <Route index element={<HomePage />} />
+      <Route path='/checkout' element={<Checkout />} />
+      <Route path='/orders' element={<Orders />} />
     </Routes>
   )
 }

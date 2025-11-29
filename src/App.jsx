ba0f8993b0a63639603cdac9edb92cfd2ee6ae82
smkarimulla <skmullakarimulla@gmail.com>
2025-11-29 17:49:48 +0530

    Implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
+import { Checkout } from './pages/Checkout'
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+      <Route path='/checkout' element={<Checkout />}></Route>
+    </Routes>
   )
 }
 
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,7 @@
+export function Checkout() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}

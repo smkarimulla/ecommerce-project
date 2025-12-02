6e6d121a58e590bdec8d664356f53b97f4bc3185
smkarimulla <skmullakarimulla@gmail.com>
2025-12-02 23:44:48 +0530

    Created Unit test for formatMoney

---
+++ src/utils/money.test.js
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+import { formatMoney } from "./money";
+
+describe('formatMoney', () => {
+    it('formats 1999 cents as $19.99', () => {
+        expect(formatMoney(1999)).toBe('$19.99');
+    });
+
+    it('Displays 2 decimals', () => {
+        expect(formatMoney(1090)).toBe('$10.90');
+        expect(formatMoney(1090)).toBe('$1.0');
+    });
+})
